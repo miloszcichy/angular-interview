5f4f717d553f79f41924914edc2168bc30448631
miloszcichy <milosz.cichy@outlook.com>
2023-01-29 11:22:43 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { combineLatest, Subject, takeUntil } from 'rxjs';
+import { combineLatest, Observable, Subject, takeUntil } from 'rxjs';
 import { Content } from '../model/content.model';
 import { User } from '../model/user.model';
 import { ContentApi } from '../service/content/content.api';
+import { DecodeService } from '../service/decode/decode.service';
 import { PermissionsApi } from '../service/permissions/permissions.api';
-import { TranslateService } from '../service/translate/translate.service';
 import { UserApi } from '../service/user/user.api';
 
 @Component({
@@ -15,25 +15,41 @@ import { UserApi } from '../service/user/user.api';
 export class AppComponent implements OnInit, OnDestroy {
   user: User = {} as User;
   content: Content[] = [] as Content[];
-  hasPermission: boolean;
+  hasPermission$: Observable<boolean>;
   private onDestroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private userApi: UserApi,
     private contentApi: ContentApi,
     private permissionsApi: PermissionsApi,
-    private translateService: TranslateService
+    private decodeService: DecodeService
   ) {}
 
   ngOnDestroy(): void {
     this.onDestroy$.next(true);
     this.onDestroy$.unsubscribe();
   }
+
   ngOnInit(): void {
+    this.getUser();
+    this.getUserPermission();
+    this.contentApi.getContent().subscribe((value) => (this.content = value));
+  }
+
+  /*1. Using userApi.getCurrentUser() fetch user and assign it to the field user.*/
+  private getUser() {
     this.userApi
       .getCurrentUser()
       .pipe(takeUntil(this.onDestroy$))
       .subscribe((value) => (this.user = value));
-    this.contentApi.getContent().subscribe((value) => (this.content = value));
+  }
+  /*2. using userApi.getCurrentUser() and permissionsApi.getPermissions()
+      create new observable "hasPermission".
+  */
+  private getUserPermission() {
+    combineLatest([
+      this.userApi.getCurrentUser(),
+      this.permissionsApi.getPermissions(),
+    ]);
   }
 }
